991c9fbaf49119523f531abf8492b1a909b2b6a1
Santiago Velosa <santiagovelosa@gmail.com>
2021-11-23 15:51:29 -0500

    Create function to add books with dynamic HTML

--- script.js
+++ script.js
@@ -4,21 +4,44 @@ let bookList = [];
 let book = {
   title : '',
   author : ''
-}
+};
 
+let booksSection = document.querySelector('.books');
 let addButton = document.querySelector('.add-book');
 let removeButton = document.querySelectorAll('.remove');
 // FUNCTIONS
 function addBook(title, author) {
-    book.title = title;
-    book.author = author;
-    bookList.push(book);
-    return bookList;
-}
+  book.title = title;
+  book.author = author;
+  bookList.push(book);
+  return bookList;
+};
 
 function removeBook(title) {
   let index = bookList.findIndex(book => book.title === title);
   bookList.splice(index, 1);
   return bookList;
-}
+};
 
+function displayBooks(bookList) {
+  for (let i = 0; i <= bookList.length; i++) {
+    return bookList[i];
+  }
+};
+
+const createElementWithClass = (type, className) => {
+  const element = document.createElement(type);
+  element.classList.add(className);
+  return element;
+};
+
+let listOfBooks = function() {
+  let book = createElementWithClass('div', 'book');
+  let bookTitle = createElementWithClass('p', 'book-author');
+  let bookAuthor = createElementWithClass('p', 'book-author');
+  bookTitle.innerHTML = book.title;
+  bookAuthor.innerHTML = book.author;
+  book.appendChild(bookTitle);
+  book.appendChild(bookAuthor);
+  booksSection.appendChild(book);
+};
