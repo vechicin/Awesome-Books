f052d7d64f7e6759b815ebd29baa2307eb198611
Santiago Velosa <santiagovelosa@gmail.com>
2021-11-23 15:23:50 -0500

    Create remove function to remove books

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+// ELEMENTS
 let bookList = [];
 
-function Book(title, author) {
-    this.title = title;
-    this.author = author;
-    bookList.push(Book);
+let book = {
+  title : '',
+  author : ''
 }
+
+let addButton = document.querySelector('.add-book');
+let removeButton = document.querySelectorAll('.remove');
+// FUNCTIONS
+function addBook(title, author) {
+    book.title = title;
+    book.author = author;
+    bookList.push(book);
+    return bookList;
+}
+
+function removeBook(title) {
+  let index = bookList.findIndex(book => book.title === title);
+  bookList.splice(index, 1);
+  return bookList;
+}
+
