14ae57fe5fba5a8a01b85bb7687375e6a3f3d5f3
Santiago Velosa <santiagovelosa@gmail.com>
2021-11-24 13:00:58 -0500

    Refactor JavaScript using classes instead of objects and arrays

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 // ELEMENTS
-// let bookList = [];
 const booksSection = document.querySelector('.books');
 const addButton = document.querySelector('.add-book');
 const listedBook = document.querySelector('.book-list');
 
 // CLASSES
-
 class Book {
   constructor(title, author) {
     this.id = new Date().getTime();
@@ -14,108 +12,46 @@ class Book {
   }
 }
 
-class Shelve {
-  constructor(){
-    bookList = [];
+class Shelf {
+  constructor() {
+    this.bookList = [];
   }
 
-  listOfBooks(title, author, id) {
-    const li = document.createElement('li');
-    li.innerHTML = `${title} by ${author} <button id="${id}">Remove</button>`;
-    listedBook.appendChild(li);
-  };
-  
-  addBook(title, author) {
-    const book = new Book(title, author)
-    this.bookList.push(book)
+  addBook(object) {
+    this.bookList.push(object);
     localStorage.setItem('BookList', JSON.stringify(this.bookList));
-  } 
+  }
 
   removeBook(id) {
-    const index = bookList.findIndex((book) => book.id === id);
-    bookList.splice(index, 1);
-    localStorage.setItem('BookList', JSON.stringify(bookList));
+    const index = this.bookList.findIndex((book) => book.id === id);
+    this.bookList.splice(index, 1);
+    localStorage.setItem('BookList', JSON.stringify(this.bookList));
   }
 
-  loadList() {
-    if (!localStorage.getItem('BookList')) {
+  checkLs() {
+    if(!localStorage.getItem('BookList')) {
       this.bookList = [];
       localStorage.setItem('BookList', JSON.stringify(this.bookList));
-      } else {
+    } else {
       this.bookList = JSON.parse(localStorage.getItem('BookList'));
-     }
-    };
-
-   loadPage() {
-      this.loadList();
-      this.bookList.forEach((element) => {
-        listedBook.insertAdjacentHTML('beforeend', `<li>${element.title} by ${element.author} <button id="${element.id}">Remove</button></li>`);
-      });
-    };
+    }
+  }
 }
 
-const bookList = new Shelve()
-console.log(bookList.bookList)
-
-
-
-// FUNCTIONS
-// const listOfBooks = function (title, author, id) {
-//   const li = document.createElement('li');
-//   li.innerHTML = `${title} by ${author} <button id="${id}">Remove</button>`;
-//   listedBook.appendChild(li);
-// };
-
-// function addBook(title, author) {
-//   const book = {
-//     id: 0,
-//     title: '',
-//     author: '',
-//   };
-//   if (title === '' || author === '') {
-//     return;
-//   }
-//   book.id = new Date().getTime();
-//   book.title = title;
-//   book.author = author;
-//   bookList.push(book);
-//   listOfBooks(book.title, book.author, book.id);
-//   localStorage.setItem('BookList', JSON.stringify(bookList));
-// }
-
-// function removeBook(id) {
-//   const index = bookList.findIndex((book) => book.id === id);
-//   bookList.splice(index, 1);
-//   localStorage.setItem('BookList', JSON.stringify(bookList));
-// }
-
-// const loadList = function () {
-//   if (!localStorage.getItem('BookList')) {
-//     bookList = [];
-//     localStorage.setItem('BookList', JSON.stringify(bookList));
-//   } else {
-//     bookList = JSON.parse(localStorage.getItem('BookList'));
-//   }
-// };
-
-// const loadPage = function () {
-//   loadList();
-//   bookList.forEach((element) => {
-//     listedBook.insertAdjacentHTML('beforeend', `<li>${element.title} by ${element.author} <button id="${element.id}">Remove</button></li>`);
-//   });
-// };
-
 // EVENT LISTENERS
+const shelf = new Shelf();
+shelf.checkLs();
+
 addButton.addEventListener('click', () => {
   const bookTitle = document.getElementById('title').value;
   const bookAuthor = document.getElementById('author').value;
-  addBook(bookTitle, bookAuthor);
+  const book = new Book(bookTitle, bookAuthor);
+  shelf.addBook(book);
+  listedBook.insertAdjacentHTML('beforeend', `<li>${book.title} by ${book.author} <button class="remove-button">Remove</button></li>`);
 });
 
 booksSection.addEventListener('click', (e) => {
-  removeBook(e.target.id);
+  Book.removeBook(e.target.id);
   const element = document.getElementById(e.target.id);
   element.parentElement.remove();
 });
-
-loadPage();
